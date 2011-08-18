8db42ce5264cbeb2ac21ed16d5ea9a0e83302ea3
Andre Oschadlin <andreoschadlin@gmail.com>
2011-08-18 08:15:16 -0700

    Added multi-curl

--- clientSimulator.php
+++ clientSimulator.php
@@ -1,15 +1,15 @@
 <?php
+  include './EpiCurl.php';
+  $mc = EpiCurl::getInstance();
+
+  // call server
   $ch = curl_init("http://localhost/main/receipt");
   curl_setopt($ch, CURLOPT_PORT, 4080);
-  curl_setopt($ch, CURLOPT_HEADER, 0);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_POST, 1);
 
-  for ($i = 0; $i<10000; $i++) {
+  for ($i = 0; $i<10; $i++) {
     $encoded = json_encode(array('count' => $i, 'bob' => 2));
     curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded);
-    $output = curl_exec($ch);
+    $mc->addCurl($ch);
   }
-  curl_close($ch);
-
 ?>
