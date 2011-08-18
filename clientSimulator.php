23b463dce59bb2f1636d3ec59aa431bc9c0c2ede
Andre Oschadlin <andreoschadlin@gmail.com>
2011-08-18 08:46:07 -0700

    Updated multicurl to work

--- clientSimulator.php
+++ clientSimulator.php
@@ -2,14 +2,23 @@
   include './EpiCurl.php';
   $mc = EpiCurl::getInstance();
 
-  // call server
-  $ch = curl_init("http://localhost/main/receipt");
-  curl_setopt($ch, CURLOPT_PORT, 4080);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$responses = $curlHandles = array();
+for ($i = 0; $i<1000; $i++) {
+ $curlHandles[$i] = curl_init("http://localhost/main/receipt");
+ curl_setopt($curlHandles[$i], CURLOPT_PORT, 4080);
+ curl_setopt($curlHandles[$i], CURLOPT_HEADER, 0);
+ curl_setopt($curlHandles[$i], CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($curlHandles[$i], CURLOPT_POST, 1);
+ $encoded = json_encode(array('count' => $i, 'bob' => 2));
+ curl_setopt($curlHandles[$i], CURLOPT_POSTFIELDS, $encoded);
+ $output = curl_exec($curlHandles[$i]);
+ $responses[] = $mc->addCurl($curlHandles[$i]);
+}
 
-  for ($i = 0; $i<10; $i++) {
-    $encoded = json_encode(array('count' => $i, 'bob' => 2));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded);
-    $mc->addCurl($ch);
-  }
+foreach($responses as $count => $response)
+ echo "Call {$count} had code {$response->code}\n<br>"; // this is needed to ensure the call completes
+
+foreach($curlHandles as $handle)
+ curl_close($handle);
+
 ?>
